476677fbf12b232709b108ebb2cf1cd3e58f229e
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-07 15:39:11 +0300

    day1

---
+++ day1demo.py
@@ -0,0 +1,80 @@
+name='mai Ali'
+print(name.find('al'))
+print(name.index('a'))
+print(name.startswith('I'))
+# print(name.i())
+print(name.replace('i','$'))
+print(name.split('a'))
+print(name)
+print(len(name))
+# print(name)
+# print(name[2],name[-1])
+# # collectionvarname[start=0:end=lengthcollectio:step=1]
+# print(name[0:5:1])
+# print(name[1:])
+# print(name[:5])
+# print(name[::-1])
+
+# print('ddf')
+# # print=1#convert fun to var int
+# print('aya')
+#
+# # d9monthtrack='pyton'
+# # n=10
+# # N='aya'
+# # print(n,N)
+# """
+# name='ahmed ali'
+# bio='''
+# dfksd;lf
+# sdklfksd;lf
+# dksf;sdf
+# '''
+# """
+#
+# #
+# # if(False):
+# #     print('eee')
+# #     if(False):
+# #         print('dd')
+# #     print('eee')
+# # print('hi')
+# # # # bool True,1,False,0
+# # # #falsy value False,0,'',"",'''''',"""""",None,(),{},[]
+# # # print(4==5)#==True comparsion operator TRue logical operator
+# # # #or return first true or last false
+# # # #and return last true or first false
+# # # print(4 and 5)
+# # # print(4 or 5)
+# # #
+# # # # num = eval(input('enter number'))
+# # # # # print(num>=5 and num<=10)
+# # # # # print(not (num>=5))
+# # # # # # num=int(num)
+# # # # print(type(num))
+# # # # print(1,1.1,'aya',True,(),{},[2,'d'],sep='$',end='---->')
+# # # # # print('ddddd')
+# # # # # print('python ')
+# # # # # print(2=='2') #compare value & data type
+# # # # #
+# # # # # # varname='value'
+# # # # # # x=10
+# # # # # # x+=1#go to memory read x vlue+10 --->save new value in x
+# # # # # # x-=1
+# # # # # # x*=2
+# # # # # # x/=2
+# # # # # # # #int,float,complex
+# # # # # # # c1=2+2j
+# # # # # # # c2=3+4j
+# # # # # # # print(c1+c2)#real+real "+" imag+img j (operator overloading)
+# # # # # # #
+# # # # # # # # import sys
+# # # # # # # # print(sys.maxsize)
+# # # # # # # # x=1
+# # # # # # # # print(type(x))
+# # # # # # # # x=1.1
+# # # # # # # # print(type(x))
+# # # # # # # # x=2+3j
+# # # # # # # # print(type(x))
+# # # # # # # # #+,-,*,/,//,%,**
+# # # # # # # # print(2**2,3**4)
