d7065716da5e4fffee9e555ca47ae7bb0ec50b87
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-12 23:34:48 +0300

    Book model

--- DjangoDemo/demo/book/models.py
+++ DjangoDemo/demo/book/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class Catagory(models.Model):
+class Catagory2(models.Model):
     id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=100,null=False)
+class Book2(models.Model):
+    id = models.AutoField(primary_key=True)
+    name=models.CharField(max_length=100,null=False)
+    publish_date=models.DateField()
+    update_date=models.DateField(auto_now=True)
+    image=models.ImageField(upload_to='books/imgs',blank=True,null=True)
+    catagory=models.ForeignKey(to=Catagory2,on_delete=models.CASCADE)
