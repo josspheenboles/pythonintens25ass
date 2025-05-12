803c1d56a71564133a0d0075a7f010a2dbda03e3
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-13 01:01:05 +0300

    insert form & pass data from view to template

--- DjangoDemo/demo/book/views.py
+++ DjangoDemo/demo/book/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from .models import Book2
+from .models import Book2,Catagory2
 from django.http import HttpResponse
 # Create your views here.
 def book_list(request):
 
     return render(request,'book/list.html')
 def book_new(request):
-    return HttpResponse('<h1>New Book</h1>')
+    catagories=Catagory2.objects.all()
+    context={}
+    context['catagories']=catagories
+    # return HttpResponse('<h1>New Book</h1>')
+    return render(request, 'book/new.html',context)
 def book_update(req,id):
     return HttpResponse(f'<h1>Book updated {id}</h1>')
 def book_delete(req,id):
