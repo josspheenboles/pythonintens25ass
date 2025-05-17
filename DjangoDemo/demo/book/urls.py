9ca2a6e948681910fe4691f7ec0d9c4d954048ed
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-17 21:39:01 +0300

    partial url

--- DjangoDemo/demo/book/urls.py
+++ DjangoDemo/demo/book/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import *
 urlpatterns=[
 path('',book_list,name='Blist'),
     path('New/',book_new,name='Bnew'),
--- DjangoDemo/demo/demo/urls.py
+++ DjangoDemo/demo/demo/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from book.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Book/',include('book.urls')),
