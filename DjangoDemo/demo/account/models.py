d7a761e59eaa6939511c2cc4b1cc8414bdc82b2b
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-19 22:24:38 +0300

    custom user

--- DjangoDemo/demo/account/models.py
+++ DjangoDemo/demo/account/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser,AbstractBaseUser
 # Create your models here.
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractUser):
     # Add custom fields
-    username=models.CharField(max_length=20)
-    bio = models.TextField(blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+    email_confirm = models.BooleanField(default=False)
