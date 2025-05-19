bb2c43fb79efc796a99b362020afb25c13226fa9
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-19 21:54:43 +0300

    account model

--- DjangoDemo/demo/account/models.py
+++ DjangoDemo/demo/account/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class Account(AbstractUser):
+    region=models.CharField(max_length=100)
