aa6ad81410b3df640aed87d297e14ebb762d0f0d
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-19 22:23:34 +0300

    email configuration

--- DjangoDemo/demo/account/models.py
+++ DjangoDemo/demo/account/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser,AbstractBaseUser
 # Create your models here.
 class CustomUser(AbstractBaseUser):
     # Add custom fields
+    username=models.CharField(max_length=20)
     bio = models.TextField(blank=True)
     birth_date = models.DateField(null=True, blank=True)
--- DjangoDemo/demo/demo/settings.py
+++ DjangoDemo/demo/demo/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import STATICFILES_DIRS, MEDIA_ROOT, MEDIA_URL
+from django.conf.global_settings import STATICFILES_DIRS, MEDIA_ROOT, MEDIA_URL, AUTH_USER_MODEL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,4 +131,12 @@ MEDIA_URL='media/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+# AUTH_USER_MODEL='account.CustomUser'
+###Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'xxx@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxx'
+AUTH_USER_MODEL = 'myaccount.CustomUser'
