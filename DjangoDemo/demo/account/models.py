f8024a549ece29fb0ef8e6817ab323c5295eeaa4
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-19 22:04:53 +0300

    custom user model

--- DjangoDemo/demo/account/models.py
+++ DjangoDemo/demo/account/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
-class Account(AbstractUser):
+class CustomUser(AbstractBaseUser):
+    # Add custom fields
     bio = models.TextField(blank=True)
     birth_date = models.DateField(null=True, blank=True)
