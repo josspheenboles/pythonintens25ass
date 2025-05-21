f114d612dbaa84357b867591e235fe99d1bae783
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-21 23:47:21 +0300

    Book serlizer

--- DjangoDemo/demo/book/api/serlizer.py
+++ DjangoDemo/demo/book/api/serlizer.py
@@ -6,3 +6,10 @@ class CatagorySerlizer(serializers.ModelSerializer):
         model =Catagory2
         fields='__all__'
 
+class BookSerlizer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name =serializers.CharField(max_length=100, null=False)
+    publish_date = serializers.DateField()
+    update_date = serializers.DateField(auto_now=True)
+    image = serializers.ImageField(upload_to='books/imgs', blank=True, null=True)
+    status = serializers.BooleanField(default=True)
